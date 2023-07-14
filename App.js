91874dec4ec79296259f255aa9c0590d7059b7eb
unknown <ghorbanpour.sahand@gmail.com>
2023-07-14 13:47:42 +0330

    Tab bar completed

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
@@ -11,6 +15,35 @@ import { useFonts as useIranyekan } from "expo-font";
 
 import { theme } from "./src/infrastructure/theme";
 import { PlantsScreen } from "./src/features/plants/screens/plants.screen";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Plants: "tree",
+  Education: "book",
+  MyPlants: "flower",
+};
+
+const Education = () => (
+  <SafeArea>
+    <Text>Education</Text>
+  </SafeArea>
+);
+const MyPlants = () => (
+  <SafeArea>
+    <Text>My plants</Text>
+  </SafeArea>
+);
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Entypo name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -31,7 +64,19 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <PlantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: "#556B2F",
+              inactiveTintColor: "gray",
+            }}
+          >
+            <Tab.Screen name="Plants" component={PlantsScreen} />
+            <Tab.Screen name="Education" component={Education} />
+            <Tab.Screen name="MyPlants" component={MyPlants} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
 
       <ExpoStatusBar style="auto" />
--- src/features/plants/screens/plants.screen.js
+++ src/features/plants/screens/plants.screen.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, FlatList, SafeAreaView } from "react-native";
+import { StyleSheet, FlatList } from "react-native";
 import styled from "styled-components/native";
 
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import { PlantInfoCard } from "../components/plant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 import { fonts } from "../../../infrastructure/theme/fonts";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
+
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
